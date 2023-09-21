b92088ad3531aeab0f7574479d422c29407734ba
tomwillow <tomwillow@qq.com>
2023-09-21 21:42:20 +0800

    remove redundant comment

--- test/test.cpp
+++ test/test.cpp
@@ -288,23 +288,3 @@ TEST(Node, DoNotStackOverFlow) {
 TEST(Vec, Base) {
     vector<Node> vec;
 }
-
-/*
-y = [   a*cos(x(1)) + b*cos(x(1)-x(2)) + c*cos(x(1)-x(2)-x(3)),
-        a*sin(x(1)) + b*sin(x(1)-x(2)) + c*sin(x(1)-x(2)-x(3)),
-        x(1)-x(2)-x(3)    ];
-
-作者：无忌不悔
-链接：https://zhuanlan.zhihu.com/p/136889381
-来源：知乎
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
-*/
-// int main() {
-//
-//    // auto a = Var("a");
-//
-//    // auto expr = Var("a") * Func("cos");
-//
-//    //cout << "dsjfk" << endl;
-//    return 0;
-//}
