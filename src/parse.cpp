0b7a5c08afd557cd8e59f84a3a1333de409f65a2
tomwillow <tomwillow@qq.com>
2023-10-06 12:01:44 +0800

    add parse() framework

---
+++ src/parse.cpp
@@ -0,0 +1,9 @@
+#include "parse.h"
+
+#include "config.h"
+
+#include <cassert>
+
+namespace tomsolver {
+
+} // namespace tomsolver
---
+++ test/parse_test.cpp
@@ -0,0 +1,13 @@
+#include "parse.h"
+
+#include "memory_leak_detection.h"
+
+#include <gtest/gtest.h>
+
+using namespace std;
+using namespace tomsolver;
+
+TEST(Parse, Base) {
+    MemoryLeakDetection mld;
+
+}
