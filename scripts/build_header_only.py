8510f4f234d394e0934f31e7e56392920988e538
tomwillow <tomwillow@qq.com>
2023-10-11 23:40:41 +0800

    write scripts. not completed

--- scripts/build_header_only.py
+++ scripts/build_header_only.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import re
 
 root_path = os.path.abspath(os.path.dirname(__file__)+"/..")
 
@@ -9,5 +10,49 @@ include_path = os.path.join(root_path, "include/tomsolver")
 if not os.path.isdir(include_path):
     os.makedirs(include_path)
 
-with open(include_path+"/tomsolver.hpp", "w") as f:
-    f.write("ddddd")
+root_file = f"{root_path}/src/tomsolver.h"
+
+
+class MyClass:
+    def __init__(self, filename):
+        with open(filename, "r", encoding="utf-8") as f:
+            lines_orig = f.readlines()
+
+        # 内部依赖项
+        self.depsInner = []
+
+        # 对外部库的依赖项
+        self.depsLib = []
+
+        self.content = []
+
+        lines = []
+        for line in lines_orig:
+            line = line.strip()
+
+            if line == "#pragma once":
+                continue
+
+            innerDep = re.match(r"#include\s+\"([a-z_.]+)\"", line)
+            if innerDep is not None:
+                self.depsInner.append(innerDep.group(1))
+                continue
+
+            libDep = re.match(r"#include\s+<([a-z_.]+)>", line)
+            if libDep is not None:
+                self.depsLib.append(libDep.group(1))
+                continue
+
+            lines.append(line)
+
+
+
+
+        print(self.depsInner)
+        print(self.depsLib)
+
+
+my = MyClass(f"{root_path}/src/subs.cpp")
+
+# with open(include_path+"/tomsolver.hpp", "w") as f:
+#     f.write("ddddd")
--- src/tomsolver.h
+++ src/tomsolver.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "config.h"
-#include "node.h"
+#include "node.h" // error_type.h math_operator.h
 #include "functions.h"
 #include "simplify.h"
 #include "diff.h"
-#include "subs.h"
-#include "symmat.h"
+#include "subs.h"   // symmat.h vars_table.h
+#include "symmat.h" // mat.h vars_table.h
+#include "parse.h"
+#include "linear.h"
+#include "nonlinear.h"
